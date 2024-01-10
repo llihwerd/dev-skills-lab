ad6a3843f95d6ec18194cbe6824d76a2bcc5155f
llihwerd <100llihwerd@gmail.com>
2024-01-09 20:24:26 -0500

    rename route and update server.js

---
+++ data/skill-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {name: 'Copywriting', proficient: true, _id: 100000},
+  {name: 'Graphic Design', proficient: true, _id: 100001},
+  {name: 'Animation', proficient: false, _id: 100002},
+  {name: 'Social Media Strategy', proficient: true, _id: 100003},
+]
+
+export { 
+	skills
+}
