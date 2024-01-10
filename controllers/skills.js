4639f5dacb905fa81e7f10a75b2831f506bfc55c
llihwerd <100llihwerd@gmail.com>
2024-01-09 22:57:42 -0500

    replace local skills data with database

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,17 @@
 
-import { skills } from '../data/skill-data.js'
+// Import the model that we exported in the Todo.js model file
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => { // skills represents the result of the query, in this case ALL skills
+    res.render('skills/index', {
+      skills: skills,
+    })
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
   })
 }
 
